13b9a5b956b9b275e668cf9bc21381a7e440db33
theguyinpink <clement.carre.prevert.pro@gmail.com>
2024-05-06 02:13:18 +0200

    Iplement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import RightSidebar from '@/components/ui/RightSidebar';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import TotalBalnceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Clément', lastName: 'Carré', email: 'clement.carre.prevert.pro@gmail.com'};
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTION
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50}, {currentBalance: 500.50}]}
+      />
+    </section>
   )
 }
 
